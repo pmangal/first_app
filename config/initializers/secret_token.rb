b55112fc21a570015d1ad9c209b565ae26595b72
U-puneetm-HP\puneetm <puneetm@PUNEETM-HP.netnumber.com>
2011-03-31 13:53:36 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'cdbce4c33c718f64dfc2001d0148350216eabb5b11a4b26c82116c2eb4f47cbb177525e21275a356b8f810cf1eba3b60f05c9232cba039763cff3821eba600b5'
